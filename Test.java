3458486c8ce86c898018c2ea7cdbac38cc35675c
kumar vikram <vikram2015cse01@gmail.com>
2019-10-29 18:48:49 +0530

    Main method holder class of Stack1.java class

---
+++ Test.java
@@ -0,0 +1,20 @@
+class Test
+{
+  public static void main(String[] args)
+  {
+    Stack1 s1 = new Stack1();
+	s1.push(23);
+	s1.push(28);
+	s1.push(232);
+	s1.push(237);
+	s1.push(263);
+	s1.push(123);
+	s1.display();
+	s1.pop();
+	s1.display();
+	s1.push(98);
+	s1.display();
+	
+   }
+}   
+	
